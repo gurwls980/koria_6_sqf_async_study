e57f702608a6a3edfa6b92f9a4b82dd37c89a52f
insssu <xmrtn210845@naver.com>
2024-06-21 17:48:49 +0900

    6/21 오후수업

--- src/ch11/components/DataTableBody/DataTableBody.jsx
+++ src/ch11/components/DataTableBody/DataTableBody.jsx
@@ -1,9 +1,36 @@
+import { SAMPLE_PRODUCTS } from "../../constants/sampleProducts";
 import "./style.css"
 
-function DataTableBody() {
+function DataTableBody( { mode, products } ) {
     return ( 
         <div className="table-body">
-
+            <table>
+                <thead>
+                    <tr>
+                        <th><input type="checkbox" disabled={mode !== 3} /></th>
+                        <th>상품코드</th>
+                        <th>싱픔명</th>
+                        <th>사이즈</th>
+                        <th>색상</th>
+                        <th>가격</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(product => (
+                            <tr key={product.id}>
+                                <th><input type="checkbox" disabled={mode === 0 || mode === 1} /></th>
+                                <td>{product.id}</td>
+                                <td>{product.productName}</td>
+                                <td>{product.size}</td>
+                                <td>{product.color}</td>
+                                <td>{product.price}</td>
+                             </tr>
+                        ))
+                    }
+                    
+                </tbody>
+            </table>
         </div>
      );
 }
--- src/ch11/components/DataTableHeader/DataTableHeader.jsx
+++ src/ch11/components/DataTableHeader/DataTableHeader.jsx
@@ -1,29 +1,55 @@
 import "./style.css"
 
-function DataTableHeader() {
+function DataTableHeader( { mode, setMode }) {
+
+    const handleChangeModeClick = (e) => {
+        setMode(parseInt(e.target.value));
+    }
+
+    const handleSubmitClick = () => {
+        if(mode === 1) {
+            alert("상품 추가")
+        };
+        if(mode === 2) {
+            alert("상품 수정")  
+        };
+        if(mode === 3) {
+            alert("상품 삭제")
+        };
+        resetMode();
+    }
+
+    const handleCancleClick = () => {
+        resetMode();
+    }
+
+    const resetMode = () => {
+        setMode(0)
+    }
+
     return ( 
         <header className="table-header">
-            <div>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+            <div className="input-group" >
+                <input type="text" disabled={mode === 0 || mode === 3} placeholder="상품명" autoFocus/>
+                <input type="text" disabled={mode === 0 || mode === 3} placeholder="사이즈" />
+                <input type="text" disabled={mode === 0 || mode === 3} placeholder="색상" />
+                <input type="text" disabled={mode === 0 || mode === 3} placeholder="가격" />
             </div>
             <div>
                 {
-                    true && (
-                        <div>
-                            <button>추가</button>
-                            <button>수정</button>
-                            <button>삭제</button>
+                    !mode && (
+                        <div className="button-group">
+                            <button  onClick={handleChangeModeClick} value={1} >추가</button>
+                            <button  onClick={handleChangeModeClick} value={2} >수정</button>
+                            <button  onClick={handleChangeModeClick} value={3} >삭제</button>
                         </div>
                     )
                 }           
                 {
-                    false && (
-                        <div>
-                            <button>확인</button>
-                            <button>취소</button>
+                    !!mode && (
+                        <div className="button-group">
+                            <button onClick={handleSubmitClick} >확인</button>
+                            <button onClick={handleCancleClick} >취소</button>
                         </div>
                     )                                    
                 }
